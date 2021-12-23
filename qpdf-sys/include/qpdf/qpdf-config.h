162c4824e6058000ae3642026e19063c9323867e
Dmitry Pankratov <dmitry@pankratov.net>
2021-12-23 17:24:40 +0100

    Actions, qpdf-config.h

--- qpdf-sys/include/qpdf/qpdf-config.h
+++ qpdf-sys/include/qpdf/qpdf-config.h
@@ -1,11 +1,12 @@
 #define DEFAULT_CRYPTO "native"
 #define USE_CRYPTO_NATIVE 1
 #define HAVE_EXTERN_LONG_TIMEZONE 1
-#ifndef _MSC_VER
-#define HAVE_FSEEKO64 1
-#endif
 #define HAVE_INTTYPES_H 1
 #define HAVE_LOCALTIME_R 1
 #define HAVE_STDINT_H 1
 #define HAVE_TM_GMTOFF 1
 #define RANDOM_DEVICE "/dev/urandom"
+
+#ifdef __linux__
+#define HAVE_FSEEKO64 1
+#endif
