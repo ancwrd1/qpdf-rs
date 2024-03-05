d481f5ce6c2a2596f7122cedcb00d205f7bbf022
Adam Reichold <adam.reichold@t-online.de>
2024-03-05 20:48:52 +0100

    Ensure vendored sources are used in the CI on Windows and macOS.

--- ci/build.bash
+++ ci/build.bash
@@ -6,16 +6,22 @@ source ci/common.bash
 
 # $1 {path} = Path to cross/cargo executable
 CROSS=$1
-# $1 {string} = <Target Triple>
+# $2 {string} = <Target Triple>
 TARGET_TRIPLE=$2
-# $3 {boolean} = Whether or not building for release or not.
-RELEASE_BUILD=$3
+# $3 {boolean} = Whether to use vendored sources.
+VENDOR=$3
+# $4 {boolean} = Whether or not building for release or not.
+RELEASE_BUILD=$4
 
 required_arg $CROSS 'CROSS'
 required_arg $TARGET_TRIPLE '<Target Triple>'
 
+if [ -n "$VENDOR" ]; then
+    VENDOR="--features vendored"
+fi
+
 if [ -z "$RELEASE_BUILD" ]; then
-    $CROSS build --target $TARGET_TRIPLE --workspace
+    $CROSS build --target $TARGET_TRIPLE $VENDOR --workspace
     $CROSS build --target $TARGET_TRIPLE --all-features --workspace
 else
     $CROSS build --target $TARGET_TRIPLE --all-features --release --workspace
